c3e3ef4e07ca09a364af7fe65838f2ab2bdc97ac
NicolasBenitez30 <nicolasbenitez1438@gmail.com>
2022-03-10 15:43:44 -0300

    algo del test

--- RepasoPersona.test/PersonaTest.cs
+++ RepasoPersona.test/PersonaTest.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using RepasoPersona.Core;
 
-namespace RepasoPersona.test
+namespace RepasoPersona.Test
 {
     public class PersonaTest
     {
-        [Fact]
-        public void Test1()
-        {
+        public Persona Pepito { get; set; }
 
+        [Fact]
+        public void Constructor()
+        {
+            Pepito = new Persona("Pepito", "Gomez", 0);
+            Assert.Equal("Pepito", Pepito.Nombre);
+            Assert.Equal("Gomez", Pepito.Apellido);
+            Assert.Equal( 0, Pepito.Efectivo);
         }
     }
 }
